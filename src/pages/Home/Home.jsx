d8e11c1a8f826ee0c4ebc5cf71c29a8720b3f413
abel <morenoabel806@gmail.com>
2026-03-11 17:48:48 -0500

    fix duplicated imports

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,19 @@
-import Header from '../../components/Header/Header.jsx'
+import Header from "../../components/Header/Header.jsx"
 import Personaje from "../../components/CardPersonaje/Personaje.jsx"
 import Historia from "../../components/Historias/Historia.jsx"
-import Footer from '../../components/Footer/Footer.jsx'
-import './Home.css'
+import Footer from "../../components/Footer/Footer.jsx"
+import "./Home.css"
 
 export default function Home() {
-
-    return(
-        <div>
-            <Header/>
-            <Historia />
-            <section className="personajes-home">
-                <h2>Personajes Icónicos de la Saga</h2>
-                <Personaje/>
-            </section>
-            <Footer />
-            
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Historia />
+      <section className="personajes-home">
+        <h2>Personajes Icónicos de la Saga</h2>
+        <Personaje />
+      </section>
+      <Footer />
+    </div>
+  )
 }
