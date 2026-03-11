b8765b1e67112dec271728233b6798359b957b9e
abel794 <morenoabel806@gmail.com>
2026-03-11 18:26:44 -0500

    Fix import path for Header component

--- src/pages/Naves/Naves.jsx
+++ src/pages/Naves/Naves.jsx
@@ -1,5 +1,5 @@
 import NavesCard from '../../components/NavesCards/NavesCards'
-import Header from '../../components/Header/Header';
+import Header from '../../components/header/Header';
 export default function Naves() {
     
   return (
