bafed5fb2d64b5b9bfdd072c2325743709308266
abel <morenoabel806@gmail.com>
2026-03-11 18:14:09 -0500

    Corregir importación de Header con mayúsculas

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Header from "../../components/Header/Header.jsx"
 import Personaje from "../../components/CardPersonaje/Personaje.jsx"
 import Historia from "../../components/Historias/Historia.jsx"
 import Footer from "../../components/Footer/Footer.jsx"
@@ -6,7 +7,7 @@ import "./Home.css"
 export default function Home() {
   return (
     <div>
-      
+      <Header />
       <Historia />
       <section className="personajes-home">
         <h2>Personajes Icónicos de la Saga</h2>
