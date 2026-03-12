0c7b040769c3a9160d17bd9dee0f22227a04a7f1
abel794 <morenoabel806@gmail.com>
2026-03-11 19:06:57 -0500

    Fix import path case for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home.jsx";
+import Home from "./pages/Home/Home.jsx";
 import Personajes from "./pages/personajes/Personajes.jsx";
 import Naves from'./pages/naves/Naves.jsx'
 import Login from "./pages/Auth/login/Login.jsx";
